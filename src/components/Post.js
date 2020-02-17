2efbb09bc0516b280de47311dcc2985310837c0a
Bhupendra <bhupendra1011@gmail.com>
2020-02-17 21:47:08 +0530

    Context Hook eases Consumer render props pattern

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,28 +2,23 @@ import React from "react";
 import { userContext } from "../App";
 
 function Post({ image, user, content }) {
+  const currentUser = React.useContext(userContext);
+  const isCurrentUser = currentUser === user;
   return (
-    <userContext.Consumer>
-      {currentUser => {
-        const isCurent = currentUser === user;
-        return (
-          <>
-            <p> {content}</p>
-            {image && (
-              <img
-                alt="post-img"
-                style={{ width: 200, height: 100, objectFit: "cover" }}
-                src={URL.createObjectURL(image)}
-              />
-            )}
-            <p>
-              Posted by :{" "}
-              <strong style={{ color: isCurent && "green" }}>{user}</strong>
-            </p>
-          </>
-        );
-      }}
-    </userContext.Consumer>
+    <>
+      <p> {content}</p>
+      {image && (
+        <img
+          alt="post-img"
+          style={{ width: 200, height: 100, objectFit: "cover" }}
+          src={URL.createObjectURL(image)}
+        />
+      )}
+      <p>
+        Posted by :{" "}
+        <strong style={{ color: isCurrentUser && "green" }}>{user}</strong>
+      </p>
+    </>
   );
 }
 
